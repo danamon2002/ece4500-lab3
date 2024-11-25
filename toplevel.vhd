70d10c6dfca9956f2c060eaba3dca4471c96b784
ted5363 <ted5363@rit.edu>
2024-11-25 12:45:30 -0500

    toplevel

---
+++ toplevel.vhd
@@ -0,0 +1,17 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity toplevel is
+	port( -- TODO: ADD INPUTS AND OUTPUTS
+	     );
+end entity toplevel;
+
+
+architecture top of toplevel is
+
+begin
+
+
+
+end toplevel;
