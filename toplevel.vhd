2eaec26c35b8ba1b792a64c919198d534de57941
calbers15 <cameron_albers@student.uml.edu>
2024-12-05 18:48:48 -0500

    added double dabble to toplevel

--- toplevel.vhd
+++ toplevel.vhd
@@ -54,6 +54,30 @@ begin
 		seven_segment(i) <= get_hex_digit(to_integer(unsigned(counter(4*i + 3 + 13 downto 4*i + 13)))); 
 	end generate set_outs;
 	
+	
+	function to_bcd (
+			data_value: in std_logic_vector(15 downto 0)
+		) return std_logic_vector
+	is
+		variable ret: std_logic_vector(19 downto 0);
+		variable temp: std_logic_vector(data_value’range);
+	begin
+		temp := data_value;
+		ret := (others => ’0’);
+		for i in data_value’range loop
+			for j in 0 to ret’length/4 - 1 loop
+				if unsigned(ret(4*j + 3 downto 4*j)) >= 5 then
+					ret(4*j + 3 downto 4*j) :=
+						std_logic_vector(
+							unsigned(ret(4*j + 3 downto 4 * j)) + 3);
+				end if;
+			end loop;
+			ret := ret(ret’high -1 downto 0) & temp(temp’high);
+			temp := temp(temp’high - 1 downto 0) & ’0’;
+		end loop;
+		return ret;
+	end function to_bcd;
+	
    -- TODO: FIGURE OUT WHAT CHANNEL SELECT IS/SHOULD BE
 --
 --	-- PLL
